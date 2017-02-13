aa6ac00449e0cacb879200dedc3a4d5be56eddea
Mengzhen <gao.mengzhen94@gmail.com>
2017-02-12 22:37:37 -0500

    step3

--- app/phone-list.component.spec.js
+++ app/phone-list.component.spec.js
@@ -1,13 +1,18 @@
-describe('PhoneListController', function(){
+
+
+describe('phoneList', function(){
 	//Before each test we tell AngularJS to load the phonecatApp module.
 	beforeEach(module('phonecatApp'));
+
+	describe('PhoneListController', function(){
+	
 	//We ask AngularJS to inject the $controller service into our test function.
 	it('should create a "phone" model with 3 phones',
-		inject(function($controller){
-			var scope = {};
+		inject(function($componentController){
 			//We use $controller to create an instance of the PhoneListController.
-			var ctrl = $controller('PhoneListController',{$scope: scope});
+			var ctrl = $componentController('phoneList');
 
-			expect(scope.phones.length).toBe(3);
+			expect(ctrl.phones.length).toBe(3);
 		}));
+	});
 });
